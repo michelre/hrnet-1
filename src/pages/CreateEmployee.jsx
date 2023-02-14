27e200013ea4bd8021335b2df38eef8e3fbdcc53
jeanchaneto <92741347+jeanchaneto@users.noreply.github.com>
2023-02-14 10:22:32 +0100

    create employee page styling

--- src/pages/CreateEmployee.jsx
+++ src/pages/CreateEmployee.jsx
@@ -1,4 +1,4 @@
-import { FormControl, InputLabel, MenuItem, Select, TextField } from "@mui/material";
+import { Button, FormControl, InputLabel, MenuItem, Select, TextField } from "@mui/material";
 import { DatePicker } from "@mui/x-date-pickers";
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
@@ -8,17 +8,16 @@ import { employeesAction } from "../redux/employeeList-slice";
 import { useDispatch } from "react-redux";
 
 
-
 const CreateEmployee = () => {
 
-    const [firstName, setFirstName] = useState(null);
-    const [lastName, setLastName] = useState(null);
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
     const [birthDate, setBirthDate] = useState(null);
     const [startDate, setStartDate] = useState(null);
-    const [street, setStreet] = useState(null);
-    const [city, setCity] = useState(null);
-    const [state, setState] = useState("");
-    const [zipCode, setZipCode] = useState(null);
+    const [street, setStreet] = useState("");
+    const [city, setCity] = useState("");
+    const [state, setState] = useState(null);
+    const [zipCode, setZipCode] = useState("");
     const [department, setDepartment] = useState(null);
 
     const dispatch = useDispatch();
@@ -39,97 +38,129 @@ const CreateEmployee = () => {
         e.preventDefault();
         dispatch(employeesAction.addEmployee(newEmployee));
         //clear states
-        setFirstName(null);
-        setLastName(null)
+        setFirstName("");
+        setLastName("")
         setBirthDate(null);
         setStartDate(null);
-        setStreet(null);
-        setCity(null);
-        setState("");
-        setZipCode(null);
+        setStreet("");
+        setCity("");
+        setState(null);
+        setZipCode("");
         setDepartment(null);
     }
 
     return (
         <>
-            <div className="title">
+            <div className="create-header">
                 <h1>HRnet</h1>
+                <Button variant="text">
+                    <NavLink to="/employee-list" > View Current Employees</NavLink></Button>
+                <h2>Create Employee</h2>
             </div>
-            <NavLink to="/employee-list" > View Current Employees</NavLink>
-            <h2>Create Employee</h2>
-            <form>
-                <label htmlFor="first-name">First Name</label>
-                <input type="text"
-                    onChange={(e) => setFirstName(e.target.value)} />
-                <label htmlFor="last-name">Last Name</label>
-                <input type="text"
-                    onChange={(e) => setLastName(e.target.value)} />
+            <form className="create-form">
+
+                <TextField
+                    className="form-field"
+                    onChange={(e) => setFirstName(e.target.value)} label="First Name"
+                    variant="outlined"
+                    value={firstName}
+                    size="small" />
+
+                <TextField
+                    className="form-field"
+                    onChange={(e) => setLastName(e.target.value)} label="Last Name"
+                    variant="outlined"
+                    value={lastName}
+                    size="small" />
 
                 <DatePicker
+                    className="form-field"
                     label="Date of Birth"
                     value={birthDate}
                     onChange={(newValue) => {
                         setBirthDate(format(newValue, "MM/dd/yyyy"));
                     }}
-                    renderInput={(params) => <TextField {...params} />}
+                    renderInput={(params) => <TextField size="small" {...params} />}
                 />
 
                 <DatePicker
+                    className="form-field"
                     label="Start Date"
                     value={startDate}
                     onChange={(newValue) => {
                         setStartDate(format(newValue, "MM/dd/yyyy"));
                     }}
-                    renderInput={(params) => <TextField {...params} />}
+                    renderInput={(params) => <TextField size="small" {...params} />}
                 />
 
+                <fieldset className="form-address-box">
+                    <h3>Address</h3>
 
-                <h3>Address</h3>
-                <label htmlFor="street">Street</label>
-                <input type="text" 
-                onChange={(e) => setStreet(e.target.value)}/>
-                <label htmlFor="city">City</label>
-                <input type="text" 
-                onChange={(e) => setCity(e.target.value)}/>
+                    <TextField
+                        className="form-field"
+                        onChange={(e) => setStreet(e.target.value)}
+                        label="Street"
+                        variant="outlined"
+                        value={street}
+                        size="small" />
 
-                <InputLabel id="state-select-label">State</InputLabel>
+                    <TextField
+                        className="form-field"
+                        onChange={(e) => setCity(e.target.value)}
+                        label="City"
+                        variant="outlined"
+                        value={city}
+                        size="small" />
+
+                    <InputLabel id="state-select-label">State</InputLabel>
+                    <Select
+                        className="form-field"
+                        labelId="state-select-label"
+                        id="state-select"
+                        value={"AL"}
+                        // label="State"
+                        size="small"
+                        onChange={(e) => {
+                            setState(e.target.value);
+                        }}
+                    >
+                        {states.map((state) => {
+                            return (
+                                <MenuItem value={state.abbreviation}
+                                    key={state.abbreviation}>
+                                    {state.name}</MenuItem>
+                            )
+                        })}
+                    </Select>
+
+                    <TextField
+                        className="form-field"
+                        onChange={(e) => setZipCode(e.target.value)}
+                        label="Zip Code"
+                        variant="outlined"
+                        value={zipCode}
+                        type="number"
+                        size="small" />
+                </fieldset>
+
+                <InputLabel id="department-select-label" className="form-label">Department</InputLabel>
                 <Select
-                    labelId="state-select-label"
-                    id="state-select"
-                    value={"AL"}
-                    label="State"
-                    onChange={(e) => {
-                        setState(e.target.value);
-                    }}
-                >
-                    {states.map((state) => {
-                        return (
-                            <MenuItem value={state.abbreviation} key={state.abbreviation}>{state.name}</MenuItem>
-                        )
-                    })}
-                </Select>
-
-                <label htmlFor="zip-code">Zip Code</label>
-                <input type="number" 
-                onChange={(e) => setZipCode(e.target.value)}/>
-
-                <InputLabel id="department-select-label">Department</InputLabel>
-                <Select
+                    className="form-field"
                     labelId="department-select-label"
                     id="department-select"
                     value={"Sales"}
-                    label="Department"
+                    // label="Department"
                     onChange={(e) => {
                         setDepartment(e.target.value);
                     }}
+                    size="small"
                 >
                     <MenuItem value="Sales">Sales</MenuItem>
                     <MenuItem value="Marketing">Marketing</MenuItem>
                     <MenuItem value="Engineering">Human Resources</MenuItem>
                     <MenuItem value="Legal">Legal</MenuItem>
                 </Select>
-
-                <button onClick={handleSave}>Save</button>
+                <Button variant="contained" className="save-button" onClick={handleSave}>Save</Button>
 
             </form>
 
